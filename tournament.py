6e739d05d84e9d9a2ef1e0b0ab64eb60e493e74d
pshevade <pshevade@gmail.com>
2015-04-01 20:15:45 -0700

    MODIFIED - tournament.py -  DELETED debug print statements,                             ADDED comments tournament.py, tournament_test.py, tournament.sql, tournament_delete.sql are all working as per the minimum passable criteria for this assignment.

--- tournament.py
+++ tournament.py
@@ -13,26 +13,11 @@ def connect():
     return psycopg2.connect("dbname=swiss_style")
 
 
-'''def executeQuery(query, value1=None, value2=None):
-    rows = ['empty']
-    DB_connection = connect()
-    cur = DB_connection.cursor()
-    if value1 is not None and value2 is None:
-        cur.execute(query, (value1,))
-    elif value1 is not None and value2 is not None:
-        cur.execute(query, (value1, value2,))
-    else:
-        cur.execute(query)
-    DB_connection.commit()
-    try:
-        rows = cur.fetchall()
-    except psycopg2.ProgrammingError:
-        print('No row to return')
-    DB_connection.close()
-    return rows
-'''
-
 def executeQuery(query, values=None):
+    """ Connect to database and execute the query that is passed
+        Parameter 'values' can be a list of n elements, or can be None,
+        in which case the query is fully described as is
+    """
     rows = ['empty']
     DB_connection = connect()
     cur = DB_connection.cursor()
@@ -49,7 +34,7 @@ def executeQuery(query, values=None):
     return rows
 
 def registerTournament(name):
-    print("Calling registerTournament - registering a tournament named: {0}".format(name))
+    """Register a tournament, of the name give by parameter 'name'"""
     query = "INSERT INTO tournaments (tournament_name) values (%s) RETURNING tournament_id;"
     values = (name,)
     row = executeQuery(query, values)
@@ -71,7 +56,7 @@ def deletePlayers():
         deleteSpecificPlayer(row[0])
 
 def deleteSpecificPlayer(player_id):
-    print("Deleting player of id {0}".format(player_id))
+    """Deletes a specific player, based on the player_id"""
     query = "DELETE FROM players where player_id = %s"
     values = (player_id, )
     executeQuery(query, values)
@@ -79,6 +64,7 @@ def deleteSpecificPlayer(player_id):
 
 
 def countPlayers():
+    """Count all players, across all tournaments"""
     print("Calling countPlayers")
     """Returns the number of players currently registered."""
     query = "SELECT count(*) from players;"
@@ -86,6 +72,7 @@ def countPlayers():
     return row[0][0]
 
 def countPlayersInTournament(tournament):
+    """Count all players in a given tournament"""
     tournament_id = getTournamentID('Default')
     query = "SELECT count(*) from tournament_contestants where tournament_id = %s"
     values = (tournament_id, )
@@ -123,19 +110,20 @@ def registerPlayer(name, tournament=None):
         else:
             # 1b. if tournament doesn't exist, create it
             tournament_id = registerTournament('Default')
-
     # 2. register player
-    print("Calling registerPlayer - registering a player named: {0}".format(name))
     query = "INSERT INTO players (player_name) values (%s) RETURNING player_id;"
     values = (name, )
     row = executeQuery(query, values)
     player_id = row[0][0]
-
     # 3. register player and tournament in tournament_contestants
     registerContestants(player_id, tournament_id)
     return player_id
 
 def getTournamentID(tournament):
+    """ Return's the tournament_id value from tournaments database when given the
+        tournament's name as the parameter 'tournament'
+        If no tournament exists of the name, returns a -1 to recognize this
+    """
     query = "select tournament_id from tournaments where tournament_name = %s"
     values = (tournament, )
     rows = executeQuery(query, values)
@@ -164,7 +152,6 @@ def playerStandings():
     tournaments = executeQuery(query)
     standings = []
     for tourney in tournaments:
-        print("For tournament: {0}".format(tourney[1]))
         query = "SELECT player_id, player_name, wins, matches from getMatchesAndWins where tournament_id = %s"
         values = (tourney[0],)
         rows = executeQuery(query, values)
@@ -175,9 +162,10 @@ def playerStandings():
     print standings;
     print("The length of the standings list is: {0}".format(len(standings)))
     if len(standings) > 1:
-        print("returning all standings")
+        # This is done for multiple tournaments
         return standings
     else:
+        # In case of just one tournament, only one list is passed
         return standings[0]
 
 
@@ -197,6 +185,7 @@ def reportMatch(winner, loser, tournament=None):
     rows = executeQuery(query, values)
 
 def getPlayerId(name):
+    """ Returns player_id based on the player's name given by parameter 'name' """
     query = "select player_id from players where player_name = %s;"
     values = (name, )
     rows = executeQuery(query, values)
@@ -205,46 +194,51 @@ def getPlayerId(name):
     else: return 'Not found'
 
 def registerContestants(player, tournament):
-    print("Calling registerContestants - To the tournament id: {0} adding a player id: {1}".format(tournament, player))
+    """ Registers the contestants per tournament
+        Done for the case when multiple tournaments exist
+    """
     value = str(player) + ', ' + str(tournament)
     query = "INSERT INTO tournament_contestants values (%s, %s);"
     values = (tournament, player, )
     executeQuery(query, values)
 
 def swissPairings(tournament=None):
-    ''' Logic:
-            1. get tournament_id
+    """ Generate the swiss pairings for a given tournament. if tournament is not
+        given, then creates swiss pairs out of the default tournament playerStandings
+        Logic:
+            1. get tournament_id and calculate total rounds and total matches possible
             2. for tournament_id, check if each player has played the same number of matches
             3. if each player has played the same number of matches, check if matches played = max
                 note:   total rounds = log(2) n         where n players
                         total matches = log(2) n x n    where n players
             4. generate swiss pairing by sorting players by standing
             5. reassemble list based on OMW in case of tie
-    '''
-
+    """
+    # 1. get tournament_id and calculate total rounds and total matches possible
     if tournament is not None:
         count_players = countPlayersInTournament(tournament)
         tournament_id = getTournamentID(tournament)
     else:
         count_players = countPlayersInTournament('Default')
         tournament_id = getTournamentID('Default')
-    print("The players in this tournament are: {0}".format(count_players))
     total_rounds = math.log(count_players, 2)
     total_matches = total_rounds * count_players
     player_pairs = []
-    print("THe Tournament id is: {0}".format(tournament_id))
+    # 2. for tournament_id, check if each player has played the same number of matches
     query = "select getMatches.player_id, getMatches.matches from getMatches where getMatches.tournament_id = %s"
     values = (tournament_id, )
     rows = executeQuery(query, values)
-    print("Raw rows with player/match info from db: {0}".format(rows))
+    # Rows contains a list of tuples (player_id, matches played)
+    # We first separate the matches played of all players into a list
     matches_played = [row[1] for row in rows]
-    print("matches played by all: {0}".format(matches_played))
-
+    # Then we check if all the elements (matches played of all players) is the same
     all_played_same_matches = all(x == matches_played[0] for x in matches_played)
     if all_played_same_matches:
+        # 3. if each player has played the same number of matches, check if matches played = max
         if matches_played[0] == total_matches:
             print("We have played all the matches possible in this Swiss Style Tournament")
         else:
+            # 4. generate swiss pairing by sorting players by standing
             query = "select getWins.player_id, players.player_name from getWins, players where players.player_id = getWins.player_id order by getWins.wins"
             rows = executeQuery(query)
             player_pairs = getPlayerPairs(rows)
@@ -252,113 +246,13 @@ def swissPairings(tournament=None):
         print("We have players who still haven't played in this round...")
         for row in rows:
             print("Player ID: {0} has played {1} matches".format(row[0], row[1]))
-    print ("The player pairs are: {0}".format(player_pairs))
     return player_pairs
 
 def getPlayerPairs(players):
-    ''' Returns a list of tuples '''
+    """ Returns a list of tuples """
     players.reverse()
     player_pairs = []
     while len(players) > 0:
         player_pairs.append((players.pop() + players.pop()))
     return player_pairs
 
-def test(tournament):
-    # 1. check if tournament exists:
-    query = "select tournament_id from tournaments where tournament_name = %s"
-    if tournament is not None:
-        values = (tournament, )
-    elif tournament is None:
-        values = ('Default', )
-    rows = executeQuery(query, values)
-    if len(rows) is not 0:
-        tournament_id = rows
-        print('Here are the rows: {0}'.format(tournament_id))
-    else:
-        print("Empty row!")
-
-def main():
-
-    registerPlayer("The Mountain", "Hand's tourney")
-    registerPlayer("Brienne of Tarth", "Hand's tourney")
-    registerPlayer("Jorah Mormont", "Hand's tourney")
-    registerPlayer("Oberyn Martell", "Hand's tourney")
-    registerPlayer("Baristan Selmy", "Hand's tourney")
-    registerPlayer("Robert Baratheon", "Hand's tourney")
-    registerPlayer("Jaime Lanister", "Hand's tourney")
-    registerPlayer("The Hound", "Hand's tourney")
-
-    registerPlayer("Prasanna Shevade")
-    registerPlayer("Gauri Jog")
-    registerPlayer("Shrikant Shevade")
-    registerPlayer("Udayan Shevade")
-
-    print("Player's standings before tournament starts")
-    playerStandings()
-
-    swissPairings()
-
-    print("Player's standings after tournament ends")
-    playerStandings()
-    '''
-
-    tournament_id = registerTournament("Hand's tourney")
-    print("- Registered tournament, id given is: {0}".format(tournament_id))
-
-    tournament_id2 = registerTournament("Trial By Combat")
-    print("- Registered tournament, id given is: {0}".format(tournament_id))
-
-    player_id = registerPlayer("The Mountain")
-    print("- Registered player, id given is: {0}".format(player_id))
-    registerContestants(player_id, tournament_id2)
-    registerContestants(player_id, tournament_id)
-
-    player_id = registerPlayer("The Hound")
-    registerContestants(player_id, tournament_id)
-    print("- Registered player, id given is: {0}".format(player_id))
-
-    player_id = registerPlayer("Loras Tyrell")
-    registerContestants(player_id, tournament_id)
-    print("- Registered player, id given is: {0}".format(player_id))
-
-    player_id = registerPlayer("Jaime Lannister")
-    registerContestants(player_id, tournament_id)
-    print("- Registered player, id given is: {0}".format(player_id))
-
-    player_id = registerPlayer("Oberyn Martell")
-    registerContestants(player_id, tournament_id2)
-
-    player_id = registerPlayer("Tyrion Lannister")
-    registerContestants(player_id, tournament_id2)
-
-    player_id = registerPlayer("Bronn Sellsword")
-    registerContestants(player_id, tournament_id2)
-
-    player_id = registerPlayer("Baristan Selmy")
-    registerContestants(player_id, tournament_id)
-
-    player_id = registerPlayer("Meryn Trant")
-    registerContestants(player_id, tournament_id)
-
-    player_id = registerPlayer("Jorah Mormont")
-    registerContestants(player_id, tournament_id2)
-
-    player_id = registerPlayer("Davos Seaworth")
-    registerContestants(player_id, tournament_id2)
-
-    print("total players are: {0}".format(countPlayers()))
-    #swissPairings()
-
-    playerStandings()
-    '''
-
-
-
-    #deleteMatches()
-    #print('The student id for {0} is {1}'.format('Prasana Shevade', id1))
-    #print('The student id for {0} is {1}'.format('Gauri Jog', id2))
-    #print('The student id for {0} is {1}'.format('Udayan Shevade', id3))
-
-
-if __name__ == '__main__':
-    main()
